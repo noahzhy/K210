137fe405ecd6987b1feeb9df0a440c02cabff0cb
noahzhy <noahzhy@gmail.com>
2020-09-02 02:13:09 +0900

    add

--- code/HTPA_32x32.py
+++ code/HTPA_32x32.py
@@ -1,4 +1,4 @@
-#import image, sensor, time, lcd, os
+import image, sensor, time, lcd, os
 import lcd, os
 from machine import I2C
 from modules import htpa
@@ -7,38 +7,40 @@ print(os.listdir())
 
 lcd.init()
 lcd.draw_string(10, 10, "hello")
+lcd.rotation(2)
 #sensor.reset()
 #sensor.set_pixformat(sensor.RGB565)
 #sensor.set_framesize(sensor.QVGA)
+#sensor.set_vflip(True)
 #sensor.run(1)
 
-# def save_img(img):
-#     path = "/sd/img_{}.jpg".format(time.localtime())
-#     img.save(path)
-#     print(len(os.listdir()))
+ #def save_img(img):
+     #path = "/sd/img_{}.jpg".format(time.localtime())
+     #img.save(path)
+     #print(len(os.listdir()))
 
 
-#def run():
-    #dev = htpa(i2c=I2C.I2C0, scl_pin=12, sda_pin=11, i2c_freq=1000000)
-    #sensor_width = dev.width()
-    #sensor_height = dev.height()
-    ##reg = dev.__write_reg(0x00d0, 15)
-    ##reg = dev.__read_reg(0xd0)
+def run():
+    dev = htpa(i2c=I2C.I2C0, scl_pin=7, sda_pin=6, i2c_freq=115200)
+    sensor_width = dev.width()
+    sensor_height = dev.height()
+    #reg = dev.__write_reg(0x00d0, 15)
+    #reg = dev.__read_reg(0xd0)
     #print(reg, type(reg))
 
-    #while True:
-        #try:
-            #dev.temperature()
-            #min, max, min_pos, max_pos = dev.min_max()
-            #if max > 6000:
-                #continue
-            #img = dev.to_image(min, max)
-            ## save to SD card
-            ## save_img(img)
-            #lcd.display(img.resize(240,240))
-        #except Exception as e:
-            #print(e)
+    while True:
+        try:
+            dev.temperature()
+            min, max, min_pos, max_pos = dev.min_max()
+            if max > 6000:
+                continue
+            img = dev.to_image(min, max)
+            # save to SD card
+            # save_img(img)
+            lcd.display(img.resize(240,240))
+        except Exception as e:
+            print(e)
 
 
-#if __name__ == "__main__":
-    #run()
+if __name__ == "__main__":
+    run()
